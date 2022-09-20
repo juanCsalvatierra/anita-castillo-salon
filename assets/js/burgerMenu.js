63be0d5af76c956018c45eb9d8b251adda9c37af
juanCsalvatierra <salvatierrajuan1951@gmail.com>
2022-09-19 21:25:31 -0300

    more animations and small fixes

--- assets/js/burgerMenu.js
+++ assets/js/burgerMenu.js
@@ -1,9 +1,17 @@
 export const burgerMenu = () => {
-	const burger = document.querySelector(".burger");
-	const container = document.querySelector(".mobile");
+	const $burger = document.querySelector(".burger");
+	const $container = document.querySelector(".mobile");
+	const $mobileAncor = document.querySelectorAll(".mobile__a");
 
-	burger.addEventListener("click", () => {
-		burger.classList.toggle("active");
-		container.classList.toggle("active");
+	$burger.addEventListener("click", () => {
+		$burger.classList.toggle("active");
+		$container.classList.toggle("active");
+	});
+
+	$mobileAncor.forEach((a) => {
+		a.addEventListener("click", () => {
+			$burger.classList.remove("active");
+			$container.classList.remove("active");
+		});
 	});
 };
